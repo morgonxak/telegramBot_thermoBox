b92c4235bb94b66d0eac49ac3aff897e63ef7f79
mar0us <40786683+mar0us@users.noreply.github.com>
2021-02-12 16:15:24 +0700

    Update updateGit.sh

--- updateGit.sh
+++ updateGit.sh
@@ -3,6 +3,6 @@ eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa_gitPull
 cd /home/pi/project/telegramBot_thermobox/
 git fetch
-git merge  --no-commit origin/testupdate --allow-unrelated-histories
+git merge  --no-commit origin/master --allow-unrelated-histories
 #svn stuff
 kill $SSH_AGENT_PID
