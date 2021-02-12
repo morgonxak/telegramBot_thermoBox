9d730687a4987880fa4814f4a0173805d1f86ca8
mar0us <40786683+mar0us@users.noreply.github.com>
2021-02-12 16:35:14 +0700

    Update settingGitUpdate.sh

--- settingGitUpdate.sh
+++ settingGitUpdate.sh
@@ -3,7 +3,6 @@ git remote rm origin
 git remote add origin git@github.com:morgonxak/telegramBot_thermoBox.git
 git config --global user.email "thermobox@mail.com"
 git config --global user.name "thermobox"
-rm README.md
 mkdir ~/.ssh/
 chmod +x settingCron.sh
 
